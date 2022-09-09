3a1b795f14a18d284d856a63236eef9850f3b704
Shin Thant <shinthant10684@gmail.com>
2022-09-10 02:30:34 +0630

    fixing-3

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
 	"http://localhost:3000",
-	"https://www.dandrepairshop.com",
-	"https://dandrepairshop.com",
+	"https://technotes-api.onrender.com",
 ];
 
 module.exports = allowedOrigins;
